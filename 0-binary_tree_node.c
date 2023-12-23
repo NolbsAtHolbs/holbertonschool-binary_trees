fd86116e48607105f0c784404e58264e7ae89f57
NolbsAtHolbs <hikikomode@gmail.com>
2023-12-22 22:53:59 -0600

    leaf check function

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -3,7 +3,7 @@
 * binary_tree_node - creates a binary tree node
 * @parent: pointer to the parent node of the node to create
 * @value: value to put in new node
-* Return: pointer to new node, or NULL on failure
+* return: pointer to new node, or NULL on failure
 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -3,7 +3,7 @@
 * binary_tree_insert_right - inserts a node as the right-child of another node
 * @parent: pointer to the node to insert the right-child in
 * @value: value to store in the new node
-* Return: pointer to created node, or NULL on failure / if parent is NULL
+* return: pointer to created node, or NULL on failure / if parent is NULL
 */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
