684c7e5768bdda0d0d8816812c275034d9a63a4b
NolbsAtHolbs <hikikomode@gmail.com>
2023-12-22 23:00:42 -0600

    root check function

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -5,7 +5,7 @@
 * binary_tree_insert_left - inserts a node as the left-child of another node
 * @parent: poionter to the node to insert the left-child in
 * @value: value to store in new node
-* return: pointer to the created node, or NULL on failure / if parent is NULL
+* Return: pointer to the created node, or NULL on failure / if parent is NULL
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
