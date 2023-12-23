26350a9f1ecdd0374401d5e4c5ff0a6c9570d2fd
NolbsAtHolbs <hikikomode@gmail.com>
2023-12-22 22:14:11 -0600

    binary tree node add left function

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+/**
+* binary_tree_insert_left - inserts a node as the left-child of another node
+* @parent: poionter to the node to insert the left-child in
+* @value: value to store in new node
+* Return: pointer to the created node, or NULL on failure / if parent is NULL
+ **/
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	if (parent->left != NULL)
+	{
+		new_node->left = parent->left;
+		new_node->left->parent = new_node;
+	}
+	parent->left = new_node;
+
+	return (new_node);
+}
