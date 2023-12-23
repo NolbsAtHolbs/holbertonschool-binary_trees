82a920f76294734c800ee343720de71bcaba2939
NolbsAtHolbs <hikikomode@gmail.com>
2023-12-22 22:30:35 -0600

    improved function

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,12 +9,14 @@
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_node = NULL
 
 	if (parent == NULL)
 	{
 		return (NULL);
 	}
+	new_node = malloc(sizeof(binary_tree_t));
+
 	if (new_node == NULL)
 	{
 		return (NULL);
@@ -31,5 +33,4 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	parent->left = new_node;
 
 	return (new_node);
-	free (new_node);
 }
